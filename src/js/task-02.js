a18091bc5883041602250ec22f267319c80b2120
Głocki Kacper <144497395+KacpeR220199@users.noreply.github.com>
2024-02-11 13:12:49 +0100

    Update task-02.js

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -1,8 +1,21 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatoes",
+  "Herbs",
+  "Condiments",
 ];
+const listIngredients = document.querySelector("#ingredients");
+
+const makeIngredient = ingredients.map((item) => {
+  const ingredientItem = document.createElement("li");
+  ingredientItem.textContent = item;
+  ingredientItem.classList.add("item");
+
+  return ingredientItem;
+});
+console.log("❗ ~ makeIngredient ~ makeIngredient:", makeIngredient);
+// <- tutaj zobaczysz że mamy tablice
+
+listIngredients.append(...makeIngredient);
