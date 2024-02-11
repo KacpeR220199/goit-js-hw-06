70f8c8c528c17a47893626d89d8f76322bf11e86
Głocki Kacper <144497395+KacpeR220199@users.noreply.github.com>
2024-02-11 19:06:16 +0100

    Delete src directory

--- src/js/task-05.js
+++
@@ -1,9 +0,0 @@
-const input = document.querySelector("#name-input");
-const output = document.querySelector("#name-output");
-
-input.addEventListener("input", (call) => {
-  output.textContent = call.currentTarget.value;
-  if (input.value.trim() === "") {
-    output.textContent = "Anonymus";
-  }
-});
