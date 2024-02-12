3cdf56a8bbce64b295c42dcfb1cd9b5d09dca725
Głocki Kacper <144497395+KacpeR220199@users.noreply.github.com>
2024-02-12 19:06:43 +0100

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -2,14 +2,14 @@ const loginForm = document.querySelector(".login-form");
 const dataObject = {};
 loginForm.addEventListener("submit", handleSubmit);
 
-function handleSubmit(loginForm) {
-  loginForm.preventDefault();
-  const form = loginForm.target;
+function handleSubmit(event) {
+  event.preventDefault();
+  const form = event.target;
   const login = form.elements.email.value.trim();
   const password = form.elements.password.value.trim();
-  const allertMessage = "Enter your login and password!!";
+  const alertMessage = "Enter your login and password!!";
   if (login === "" || password === "") {
-    allert(allertMessage);
+    alert(alertMessage);
   } else {
     dataObject.login = login;
     dataObject.password = password;
